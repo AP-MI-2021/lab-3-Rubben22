239d4e001c57ade2f6d4de7bbf45757496fa6999
[Rubben22] <pasca.ruben120@gmail.com>
2021-10-11 20:50:16 +0300

    Update

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def cifre_prime(n):
     return True
 
 
-def get_longest_prime_digits(list):
+def verificare_cifre_prime(list):
     """
     Functia verifica daca toate numerele sunt formate din cifre prime
     :param list: lista de numere intregi
@@ -73,10 +73,19 @@ def get_longest_prime_digits(list):
     return True
 
 
+def get_longest_prime_digits(list):
+    secventaMax = []
+    for i in range(len(list)):
+        for j in range(i, len(list)):
+            if verificare_cifre_prime(list[i:j + 1]) and len(list[i:j + 1]) > len(secventaMax):
+                secventaMax = list[i:j + 1]
+    return secventaMax
+
+
 def test_get_longest_prime_digits():
-    assert get_longest_prime_digits([2, 22, 75]) is True
-    assert get_longest_prime_digits([61, 7]) is False
-    assert get_longest_prime_digits([83, 77, 55, 5]) is False
+    assert get_longest_prime_digits([2, 22, 18, 75]) == [2, 22]
+    assert get_longest_prime_digits([61, 7]) == [7]
+    assert get_longest_prime_digits([83, 77, 55, 5]) == [77, 55, 5]
 
 
 def toate_nr_la_puterea_k(list, k):
@@ -90,16 +99,25 @@ def toate_nr_la_puterea_k(list, k):
     return True
 
 
-def get_longest_powers_of_k(list, k):
+def verificare_nr_la_k(list, k):
     if not toate_nr_la_puterea_k(list, k):
         return False
     return True
 
 
+def get_longest_powers_of_k(list, k):
+    secventaMax = []
+    for i in range(len(list)):
+        for j in range(i, len(list)):
+            if verificare_nr_la_k(list[i:j + 1], k) and len(list[i:j + 1]) > len(secventaMax):
+                secventaMax = list[i:j + 1]
+    return secventaMax
+
+
 def test_get_longest_powers_of_k():
-    assert get_longest_powers_of_k([4, 36, 25], 2) is True
-    assert get_longest_powers_of_k([27, 125], 3) is True
-    assert get_longest_powers_of_k([16, 18, 27], 2) is False
+    assert get_longest_powers_of_k([4, 36, 12, 25], 2) == [4, 36]
+    assert get_longest_powers_of_k([27, 125], 3) == [27, 125]
+    assert get_longest_powers_of_k([16, 18, 27], 2) == [16]
 
 
 def Citire_lista():
@@ -114,8 +132,8 @@ def Citire_lista():
 def printMenu():
     print("1.Citire date ")
     print("2.Determina cea mai lunga secventa in care numerele sunt prime ")
-    print("3.Verifica daca toate numerele sunt formate din cifre prime ")
-    print("4.Verifica daca toate numerele se pot scrie ca x**k, k citit, x întreg pozitiv ")
+    print("3.Determina cea mai lunga secventa in care numerele sunt formate din cifre prime")
+    print("4.Determina cea mai lunga secventa in care numerele se pot scrie ca x**k, k citit, x întreg pozitiv ")
     print("5.Iesi")
 
 
@@ -132,16 +150,10 @@ def main():
         elif optiune == "2":
             print(get_longest_all_primes(list))
         elif optiune == "3":
-            if get_longest_prime_digits(list):
-                print("Toate numerele sunt formate din cifre prime ")
-            else:
-                print("Nu toate numerele sunt formate doar din cifre prime ")
+            print(get_longest_prime_digits(list))
         elif optiune == "4":
             k = int(input("dati numarul k: "))
-            if get_longest_powers_of_k(list, k):
-                print("Toate se pot scrie ")
-            else:
-                print("NU toate se pot scrie ")
+            print(get_longest_powers_of_k(list, k))
         elif optiune == "5":
             break
         else:
